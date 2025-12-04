e1f496e973699819727bf3d1051ac78d81f2d0c9
Radv2005 <U202312318@upc.edu.pe>
2025-12-04 01:12:05 -0500

    feat: add link

--- src/app/config/links.ts
+++ src/app/config/links.ts
@@ -1,3 +1,3 @@
 export const GOOGLE_PLAY_URL = "https://play.google.com/store/apps";
 export const ABOUT_TEAM_VIDEO_URL = "https://www.youtube.com/embed/VIDEO_ID";
-export const ABOUT_TEAM_PRODUCT_URL = "https://www.youtube.com/embed/VIDEO_ID";
+export const ABOUT_TEAM_PRODUCT_URL = "https://youtu.be/2QE6zWPj_JU";
