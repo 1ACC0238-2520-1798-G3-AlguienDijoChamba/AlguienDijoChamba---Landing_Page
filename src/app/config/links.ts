fc541b3cfc35dd9817990c79a4295d04c02a5f47
Radv2005 <U202312318@upc.edu.pe>
2025-12-04 04:45:00 -0500

    fix: new links

--- src/app/config/links.ts
+++ src/app/config/links.ts
@@ -1,3 +1,3 @@
 export const GOOGLE_PLAY_URL = "https://play.google.com/store/apps";
-export const ABOUT_TEAM_VIDEO_URL = "https://youtu.be/MB2a6L0Ysic";
-export const ABOUT_TEAM_PRODUCT_URL = "https://youtu.be/2QE6zWPj_JU";
+export const ABOUT_TEAM_VIDEO_URL = "https://www.youtube.com/embed/2QE6zWPj_JU";
+export const ABOUT_TEAM_PRODUCT_URL = "https://www.youtube.com/embed/MB2a6L0Ysic";
